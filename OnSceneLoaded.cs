c0481d52482dc894d71fc8133e58ab0d1d08cf92
TheMysticSword <26709843+TheMysticSword@users.noreply.github.com>
2024-12-06 06:23:42 +0500

    Fix misplaced argument

--- OnSceneLoaded.cs
+++ OnSceneLoaded.cs
@@ -32,7 +32,7 @@ namespace Localyssation
                         {
                             if (text.text == "Loading...")
                             {
-                                LangAdjustables.RegisterText(text, LangAdjustables.GetStringFunc("GAME_LOADING"), text.text);
+                                LangAdjustables.RegisterText(text, LangAdjustables.GetStringFunc("GAME_LOADING", text.text));
                                 text.alignment = TextAnchor.MiddleRight;
                             }
                         }
