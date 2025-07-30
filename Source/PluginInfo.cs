9ef28adcb89ac49bd416eccd1b48239f4ba4b7d9
TowardTheStars <hty16@mail.ustc.edu.cn>
2025-07-31 02:16:50 +0800

    版本号 2.2.0

--- Source/PluginInfo.cs
+++ Source/PluginInfo.cs
@@ -4,6 +4,6 @@ namespace Localyssation
     {
         public const string PLUGIN_GUID = "org.sallys-workshop.localyssation";
         public const string PLUGIN_NAME = "Localyssation";
-        public const string PLUGIN_VERSION = "2.1.4";   // Auto update by build task `ReplaceVersionText`
+        public const string PLUGIN_VERSION = "2.2.0";   // Auto update by build task `ReplaceVersionText`
     }
 }
